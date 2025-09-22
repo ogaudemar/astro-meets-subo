466214c408dbc9b8f5800f8ddac20119fdcf54c9
ohdeegee <odegaudemar@gmail.com>
2025-09-21 18:56:39 -0700

    Sunday enhancements

---
+++ src/config/redirects.js
@@ -0,0 +1,53 @@
+export const trackedRedirects = {
+  support: {
+    url: 'https://discord.gg/SvX6KdEUnu',
+    name: 'discord_support',
+    description: 'Discord Support Server'
+  },
+  invite: {
+    url: 'https://discord.com/api/oauth2/authorize?client_id=946888973035380806&permissions=398090292240&scope=bot%20applications.commands',
+    name: 'discord_bot_invite',
+    description: 'Discord Bot Invite'
+  },
+  portal: {
+    url: 'https://billing.stripe.com/p/login/7sI3el1F61iu5RS8ww',
+    name: 'stripe_portal',
+    description: 'Stripe Customer Portal'
+  },
+  tutorial: {
+    url: 'https://suboai.notion.site/Tutorials-831cbee01f004a81ac5fd3ae72f09bce',
+    name: 'tutorials',
+    description: 'Notion Tutorials'
+  },
+  permissions: {
+    url: 'https://suboai.notion.site/Permissions-for-SurveyBot-2a976a73aa5a4f2c851f177213656724',
+    name: 'permissions',
+    description: 'Notion Permissions'
+  },
+  xp: {
+    url: 'https://suboai.notion.site/XP-system-XP-and-roles-for-participants-acb28a7acb6c476cbfaafd0ad6c3149d',
+    name: 'understanding_xp',
+    description: 'Notion Understanding XP'
+  },
+  topgg: {
+    url: 'https://top.gg/bot/946888973035380806',
+    name: 'topgg_page',
+    description: 'Top.gg Bot Page'
+  },
+  vote: {
+    url: 'https://top.gg/bot/946888973035380806/vote',
+    name: 'topgg_vote',
+    description: 'Top.gg Vote Page'
+  },
+  upgrade: {
+    url: 'https://discord.com/discovery/applications/946888973035380806/store',
+    name: 'discord_upgrade',
+    description: 'Discord Upgrade Page'
+  },
+  billingportal: {
+    url: 'https://billing.stripe.com/p/login/7sI3el1F61iu5RS8ww',
+    name: 'stripe_billing_portal',
+    description: 'Stripe Billing Portal'
+  }
+
+};
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Subo, the Survey Bot";
-export const SITE_DESCRIPTION = "Get to know your community with conversational surveys and polls";
+export const SITE_DESCRIPTION = "Get to know your community with conversational surveys and polls in Discord";
